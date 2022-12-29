626a1ac868454fd8eb16651e19cfe1f7504b6164
sunmin <sunm309@naver.com>
2022-12-29 17:56:10 +0900

    fix: eslint, prettier, tsconfig 옵션 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: false,
 };
